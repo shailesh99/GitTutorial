3e53e46b0114e506ff2438847aef76b8d7087a8b
shailesh99 <shailesh99@gmail.com>
2013-08-25 19:39:00 -0500

    First Commit!

---
+++ src/Class1.java
@@ -0,0 +1,18 @@
+
+public class Class1 {
+	
+	public Class1(){
+		System.out.println("Class 1");
+	}
+	
+	public void printHello(){
+		System.out.println("Hello!");
+	}
+
+	public static void main(String[] args) {
+		
+		Class1 c = new Class1();
+		c.printHello();
+	}
+
+}

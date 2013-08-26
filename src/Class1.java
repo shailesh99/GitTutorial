946acfa409dcc42a4e4c5c6b741dc399c392a268
shailesh99 <shailesh99@gmail.com>
2013-08-25 19:42:23 -0500

    Second commit after adding Class2.

--- src/Class1.java
+++ src/Class1.java
@@ -1,16 +1,16 @@
-
 public class Class1 {
-	
-	public Class1(){
+
+	public Class1() {
 		System.out.println("Class 1");
 	}
-	
-	public void printHello(){
+
+	public void printHello() {
 		System.out.println("Hello!");
 	}
 
+	// Comment
 	public static void main(String[] args) {
-		
+
 		Class1 c = new Class1();
 		c.printHello();
 	}
